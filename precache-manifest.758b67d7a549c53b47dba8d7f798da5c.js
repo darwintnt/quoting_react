aa998e547b7f16612f7b6cb897bfed58201081d4
Darwin Gomez <desarrollador@fhuryx.com>
2019-05-09 19:07:05 -0500

    Updates

---
+++ precache-manifest.758b67d7a549c53b47dba8d7f798da5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81b44f8dcaaed28803317aad99ccfe0f",
+    "url": "/quoting_react/index.html"
+  },
+  {
+    "revision": "4dd8cb25431862ff6c4e",
+    "url": "/quoting_react/static/css/2.424bd964.chunk.css"
+  },
+  {
+    "revision": "a9b04e0546c0f2d17db4",
+    "url": "/quoting_react/static/css/main.02b47bd8.chunk.css"
+  },
+  {
+    "revision": "4dd8cb25431862ff6c4e",
+    "url": "/quoting_react/static/js/2.10a48ee3.chunk.js"
+  },
+  {
+    "revision": "a9b04e0546c0f2d17db4",
+    "url": "/quoting_react/static/js/main.6da1ef68.chunk.js"
+  },
+  {
+    "revision": "b5ff70068b729c572548",
+    "url": "/quoting_react/static/js/runtime~main.53355c37.js"
+  }
+]);
